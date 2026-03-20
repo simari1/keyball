6c693a221f0c6e016d8b04892d2066c232ecb6cf
simari <bdfsg610@yahoo.co.jp>
2026-03-20 10:16:08 +0900

    　change aml logic, shortcuts not end aml

--- qmk_firmware/keyboards/keyball/lib/keyball/keyball.c
+++ qmk_firmware/keyboards/keyball/lib/keyball/keyball.c
@@ -742,6 +742,13 @@ bool process_record_kb(uint16_t keycode, keyrecord_t *record) {
         set_auto_mouse_timeout(keyball_get_auto_mouse_timeout());
         keyball.total_mouse_movement = 0;
     }
+    // Keep auto mouse layer from deactivating when using shortcuts such as Ctrl+C/Ctrl+V.
+    if (record->event.pressed && layer_state_cmp(layer_state, AUTO_MOUSE_DEFAULT_LAYER) && (get_mods() & MOD_MASK_CTRL)) {
+        if (keycode >= KC_A && keycode <= KC_Z) {
+            set_auto_mouse_timeout(get_auto_mouse_keep_time());
+            keyball.total_mouse_movement = 0;
+        }
+    }
 #endif
 
     switch (keycode) {
