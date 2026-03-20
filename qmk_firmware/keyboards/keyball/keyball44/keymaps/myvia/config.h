11101d3d3328e12ecc4f4d6e11443b09dc111eab
simari <bdfsg610@yahoo.co.jp>
2026-03-20 12:11:23 +0900

    Change AML default layer

--- qmk_firmware/keyboards/keyball/keyball44/keymaps/myvia/config.h
+++ qmk_firmware/keyboards/keyball/keyball44/keymaps/myvia/config.h
@@ -37,7 +37,6 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 #define POINTING_DEVICE_AUTO_MOUSE_ENABLE
 
-#define AUTO_MOUSE_DEFAULT_LAYER 1
-#define AUTO_MOUSE_LAYER_KEEP_TIME 30000
+#define AUTO_MOUSE_DEFAULT_LAYER 4#define AUTO_MOUSE_LAYER_KEEP_TIME 30000
 #define DYNAMIC_KEYMAP_LAYER_COUNT 5
 #define TAPPING_TERM 250
