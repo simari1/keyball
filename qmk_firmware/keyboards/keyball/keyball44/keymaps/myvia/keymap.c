91a233929ec7751d7bcf01088f149bfbec941f87
simari <bdfsg610@yahoo.co.jp>
2026-05-09 20:23:11 +0900

    combo

--- qmk_firmware/keyboards/keyball/keyball44/keymaps/myvia/keymap.c
+++ qmk_firmware/keyboards/keyball/keyball44/keymaps/myvia/keymap.c
@@ -80,13 +80,3 @@ void oledkit_render_info_user(void) {
     keyball_oled_render_layerinfo();
 }
 #endif
-
-#ifndef MAGIC_ENABLE
-uint16_t keycode_config(uint16_t keycode) {
-    return keycode;
-}
-
-uint8_t mod_config(uint8_t mod) {
-    return mod;
-}
-#endif
